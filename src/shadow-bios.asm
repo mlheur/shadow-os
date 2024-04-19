50ef234ee923d3bfaea3628dd786598c0e361d96
Marc L'Heureux <marc_lh@hotmail.com>
2024-04-19 14:22:01 -0700

    feat: hex(ax)>COM1

--- src/shadow-bios.asm
+++ src/shadow-bios.asm
@@ -16,16 +16,59 @@ init:
 
 testcom1:
   pusha
-  mov ax,'M'
   mov dx,COM1
-  call comout
+  mov ax,0xFADE
+  call axout
   popa
+  ret
+
+axout:
+  ; use bx to hold ax for reuse
+  push bx
+  mov bx,ax
+
+  ; print upper 4 bits
+  and ax,0xF000
+  shr ax,12
+  call hexal4out
+
+  ; print next 4 bits
+  mov ax,bx
+  and ax,0x0F00
+  shr ax,8
+  call hexal4out
+
+  ; print next 4 bits
+  mov ax,bx
+  and ax,0x00F0
+  shr ax,4
+  call hexal4out
+
+  ; print last 4 bits
+  mov ax,bx
+  and ax,0x000F
+  call hexal4out
+
+  ; restore ax
+  mov ax,bx
+  ; restore bx
+  pop bx
+
+  ret
+
+hexal4out:
+  and al,0x0F
+  add al,0x90
+  daa
+  adc al,0x40
+  daa
+  call comout
   ret
 
 comout:
   pusha
-  ; eax has the byte to write
-  ; edx has the CPU IO address
+  ; ax has the byte to write
+  ; dx has the CPU IO address
   out dx,ax
   popa
   ret
