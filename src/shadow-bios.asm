2812f5997c8553562611c0bfa21bbf8e8a100f86
Marc L'Heureux <marc_lh@hotmail.com>
2024-04-18 21:23:42 -0700

    style: two spaces

--- src/shadow-bios.asm
+++ src/shadow-bios.asm
@@ -2,20 +2,20 @@ bits 16
 org 0xFFFF0000
 
 section .biosmain
-	db	'(C) mlheur@gmail.com'
-	init:
-		mov eax,0xFEDC
-		mov ebx,0xBA09
-		mov ecx,0x8765
-		mov edx,0x4321
-		hlt
-	
+  db  '(C) mlheur@gmail.com'
+  init:
+    mov eax,0xFEDC
+    mov ebx,0xBA09
+    mov ecx,0x8765
+    mov edx,0x4321
+    hlt
+  
 padding:
-		times 0xfff0-($-$$) hlt
+    times 0xfff0-($-$$) hlt
 
 bits 16
 section .rvec
-	rvec:
-		jmp word 0xFFFF0000+init
-		times 0x10-1-(($-$$)) hlt ; byte padding
-		db 0x00
+  rvec:
+    jmp word 0xFFFF0000+init
+    times 0x10-1-(($-$$)) hlt ; byte padding
+    db 0x00
--- src/shadow-boot.asm
+++ src/shadow-boot.asm
@@ -1,127 +1,127 @@
 [org 0x7c00] ; MBR start address
 
 section .text
-	bootloader:
-		call printrom
-		call crlf
-		call printFirstCall
-		jmp poweroff
-	
-	szout:
-		pusha
-		loadchar:
-			mov al,[esi]
-			cmp al,0
-			jne charout
-		popa
-		ret
-		charout:
-			mov ah,0x0e
-			int 0x10
-			inc esi
-			jmp loadchar
-	
-	hexout:
-		; How to convert hex to ascii in 8086 assembly
-		; https://www.wdj-consulting.com/blog/hex2ascii-daa/
-		and al,0x0f
-		add al,0x90
-		daa
-		adc al,0x40
-		daa
-		mov ah,0x0e
-		int 0x10
-		ret
-	
-	printbreg:
-		; ebx contains the thing to print
-		; ecx contains the qty of bits to print
-		pusha
-		nextb:
-			sub cl,4
-			mov eax,ebx
-			shr eax,cl
-			call hexout
-			cmp ecx,0
-			jne nextb
-		popa
-		ret
-	
-	space:
-		mov ax,0x0e20
-		int 0x10
-		ret
-	
-	crlf:
-		mov ax,0x0e0d
-		int 0x10
-	lf:
-		mov ax,0x0e0a
-		int 0x10
-		ret
-	
-	printFirstCall:
-		; ROM:FFFFFFF0  EA 5B E0 00 F0 30 36 2F  32 33 2F 39 39 00 FC 00
-		pusha
-		mov esi,0x000FFF80	; print start address
-		mov edi,0x00100000	; print end address
-		call printram
-		popa
-		ret
+  bootloader:
+    call printrom
+    call crlf
+    call printFirstCall
+    jmp poweroff
+  
+  szout:
+    pusha
+    loadchar:
+      mov al,[esi]
+      cmp al,0
+      jne charout
+    popa
+    ret
+    charout:
+      mov ah,0x0e
+      int 0x10
+      inc esi
+      jmp loadchar
+  
+  hexout:
+    ; How to convert hex to ascii in 8086 assembly
+    ; https://www.wdj-consulting.com/blog/hex2ascii-daa/
+    and al,0x0f
+    add al,0x90
+    daa
+    adc al,0x40
+    daa
+    mov ah,0x0e
+    int 0x10
+    ret
+  
+  printbreg:
+    ; ebx contains the thing to print
+    ; ecx contains the qty of bits to print
+    pusha
+    nextb:
+      sub cl,4
+      mov eax,ebx
+      shr eax,cl
+      call hexout
+      cmp ecx,0
+      jne nextb
+    popa
+    ret
+  
+  space:
+    mov ax,0x0e20
+    int 0x10
+    ret
+  
+  crlf:
+    mov ax,0x0e0d
+    int 0x10
+  lf:
+    mov ax,0x0e0a
+    int 0x10
+    ret
+  
+  printFirstCall:
+    ; ROM:FFFFFFF0  EA 5B E0 00 F0 30 36 2F  32 33 2F 39 39 00 FC 00
+    pusha
+    mov esi,0x000FFF80  ; print start address
+    mov edi,0x00100000  ; print end address
+    call printram
+    popa
+    ret
 
-	printrom:
-		pusha
-		mov esi,0xFFFFFF80
-		mov edi,0x00000000
-		call printram
-		popa
-		ret
+  printrom:
+    pusha
+    mov esi,0xFFFFFF80
+    mov edi,0x00000000
+    call printram
+    popa
+    ret
 
-	printram:
-		; esi contains the start address
-		; edi contains the end address
-		pusha
-		nextaddr:
-			; print the addr
-			mov ebx,esi
-			mov ecx,32
-			call printbreg
-			call space
-			
-			mov edx,0x10 ; entries per line
-			nextbyte:
-				call space
+  printram:
+    ; esi contains the start address
+    ; edi contains the end address
+    pusha
+    nextaddr:
+      ; print the addr
+      mov ebx,esi
+      mov ecx,32
+      call printbreg
+      call space
+      
+      mov edx,0x10 ; entries per line
+      nextbyte:
+        call space
 
-				; print the data
-				mov ebx,[esi]
-				mov ecx,0x08 ; bits per entry
-				call printbreg
+        ; print the data
+        mov ebx,[esi]
+        mov ecx,0x08 ; bits per entry
+        call printbreg
 
-				; after the halfway point, print an extra space
-				cmp edx,0x09
-				jne doincrement
-				call space
+        ; after the halfway point, print an extra space
+        cmp edx,0x09
+        jne doincrement
+        call space
 
-				doincrement:
-					inc esi
-					dec edx
-					cmp edx,0
-					jne nextbyte
-			call crlf
-			cmp esi,edi
-			jne nextaddr
-		popa
-		ret
+        doincrement:
+          inc esi
+          dec edx
+          cmp edx,0
+          jne nextbyte
+      call crlf
+      cmp esi,edi
+      jne nextaddr
+    popa
+    ret
 
-	poweroff:
-		call space
-		mov ah,0x53
-		mov al,0x07
-		mov bx,0x0001
-		mov cx,0x0003
-		int 0x15
-	
-	padding:
-		times 510-($-$$) db 0 ; byte padding
-	magicnum:
-		dw 0xaa55 ; MBR CKSUM
+  poweroff:
+    call space
+    mov ah,0x53
+    mov al,0x07
+    mov bx,0x0001
+    mov cx,0x0003
+    int 0x15
+  
+  padding:
+    times 510-($-$$) db 0 ; byte padding
+  magicnum:
+    dw 0xaa55 ; MBR CKSUM
