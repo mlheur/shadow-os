4dc69775557b10166e22469a6df85061287374b9
Marc L'Heureux <marc_lh@hotmail.com>
2024-04-19 10:37:10 -0700

    style: whitespace

--- src/shadow-bios.asm
+++ src/shadow-bios.asm
@@ -5,19 +5,19 @@ org BIOSSEGMENT
 
 section .biosmain
   db  'shadow-bios'
-  init:
-    mov eax,0xFEDC
-    mov ebx,0xBA09
-    mov ecx,0x8765
-    mov edx,0x4321
-    hlt
+init:
+  mov eax,0xFEDC
+  mov ebx,0xBA09
+  mov ecx,0x8765
+  mov edx,0x4321
+  hlt
   
 padding:
-    times 0xfff0-($-$$) hlt
+  times 0xfff0-($-$$) hlt
 
 bits 16
 section .rvec
-  rvec:
-    jmp word BIOSSEGMENT+init
-    times 0x10-1-(($-$$)) hlt ; byte padding
-    db 0x00
+rvec:
+  jmp word BIOSSEGMENT+init
+  times 0x10-1-(($-$$)) hlt ; byte padding
+  db 0x00
