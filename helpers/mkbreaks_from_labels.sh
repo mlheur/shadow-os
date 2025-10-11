92496fdb2faa24f2eb0be42137d11150e2fa6064
mlheur <mlheur@gmail.com>
2025-10-10 20:38:17 -0700

    debugging

--- helpers/mkbreaks_from_labels.sh
+++ helpers/mkbreaks_from_labels.sh
@@ -1,2 +1,2 @@
-grep : src/mbr/*.S /dev/null | awk -F : '{print $2}' | egrep -e '^[_a-zA-Z0-9]+$' 2>/dev/null | sed -e 's/^/            "break *0x7c00+/' -e 's/$/",/'
-grep : src/krn/*.S /dev/null | awk -F : '{print $2}' | egrep -e '^[_a-zA-Z0-9]+$' 2>/dev/null | sed -e 's/^/            "break *0x10200+/' -e 's/$/",/'
+grep : src/mbr/*.S /dev/null | awk -F : '{print $2}' | egrep -e '^[_a-zA-Z][_a-zA-Z0-9]+$' 2>/dev/null | sed -e 's/^/            "break *0x7c00+/' -e 's/$/",/'
+grep : src/krn/*.S /dev/null | awk -F : '{print $2}' | egrep -e '^[_a-zA-Z][_a-zA-Z0-9]+$' 2>/dev/null | sed -e 's/^/            "break *0x10200+/' -e 's/$/",/'
